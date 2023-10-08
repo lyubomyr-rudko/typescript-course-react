6920566539728991677477c06b9b23c8c4ae8227
spodolyak <sergey.podolyak@etring.com>
2023-10-08 18:39:27 +0300

    Home work 12 Sergey P

--- src/lesson12/Users-homework.tsx
+++ src/lesson12/Users-homework.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import usersData from "../users-data";
 import { TUser } from "../users-data";
 
@@ -6,11 +7,18 @@ interface IUserProps {
   data: {
     firstName: string;
     lastName: string;
+    id:number
   };
+  addLikeCalback:(id:number)=>void,
+  deleteCalback:(id:number)=>void,
+  up:(id:number)=>void,
+  down:(id:number)=>void,
+  isLiked:boolean
 }
 
 const User = (props: IUserProps) => {
-  const { data } = props;
+  const { data, addLikeCalback, deleteCalback, isLiked, up, down } = props;
+
 
   // TODO: add delete button to each of the user
   // TODO: implement logic to delete user
@@ -21,25 +29,58 @@ const User = (props: IUserProps) => {
   // TODO: display hart icon if user is liked (💝)
 
   return (
-    <li>
-      <span>
-        {data.firstName} {data.lastName}
+    <li className="userRow">
+      <span className="userName">
+        {data.firstName} {data.lastName} {isLiked?<i>💝</i>:''}
       </span>
+      <span className="firstButton"><button onClick={()=>addLikeCalback(data.id)}>Like</button></span>
+      <span><button onClick={()=>deleteCalback(data.id)}>Delete</button></span>
+      <span><button onClick={()=>up(data.id)}>Up</button></span>
+      <span><button onClick={()=>down(data.id)}>Down</button></span>
     </li>
   );
 };
 
 export function Users() {
   const [users, setUsers] = React.useState<TUser[]>(usersData);
+  const [likedUsers, setLikedUsers] = React.useState<number[]>([]);
 
-  const handleDelete = (user: TUser) => {
-    console.log("deleting", user);
+  const handleDelete = (userId: number) => {
+    console.log("deleting", userId);
     // TODO: implement function to delete user
     // TODO: create new list of users without deleted user
     // TODO: call setUsers with new list of users
-  };
-  console.log(handleDelete, setUsers);
+    const newUsers:TUser[] = users.filter(el=>el.id !== userId)
+    setUsers(newUsers);
 
+  };
+  const handleLike = (userId: number) => {
+    console.log("handleLike", userId);
+    const newLikes:number[]=[];
+    if(likedUsers.indexOf(userId)>-1){
+      newLikes.push(...likedUsers.filter(el=>el !== userId))
+    }else{
+      newLikes.push(userId, ...likedUsers);
+    }
+    console.log(newLikes);
+    setLikedUsers(newLikes);
+
+  };
+
+  const moveUp = (userId: number) => {
+    console.log("moveUp", userId);
+    const index = users.findIndex(el=>el.id === userId);
+    const neusers = [users[index],...users.filter(el=>el.id !== userId)]
+    setUsers(neusers);
+  };
+
+  const moveDown = (userId: number) => {
+    console.log("moveDown", userId);
+    const index = users.findIndex(el=>el.id === userId);
+    const neusers = [...users.filter(el=>el.id !== userId),users[index]]
+    setUsers(neusers);
+
+  };
   // TODO: pass handleDelete to User component
 
   // TODO: Add "Move Up" and "Move Down" buttons to each of the user
@@ -51,7 +92,14 @@ export function Users() {
   return (
     <ul>
       {users.map((user) => (
-        <User data={user} key={user.id} />
+        <User 
+          data={user} 
+          deleteCalback={handleDelete} 
+          addLikeCalback={handleLike} 
+          up ={moveUp} 
+          down ={moveDown} 
+          key={user.id} 
+          isLiked={likedUsers.indexOf(user.id)>-1}/>
       ))}
     </ul>
   );
