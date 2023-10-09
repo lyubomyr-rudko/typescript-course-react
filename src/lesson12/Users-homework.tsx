1796f4a6ba57fc19bacb54664422eb23c5fd5517
Vozniuk Serhii <s.voznyuk18@gmail.com>
2023-10-09 12:56:44 +0300

    done_HW_12_Vozniuk_Serhii

--- src/lesson12/Users-homework.tsx
+++ src/lesson12/Users-homework.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import usersData from "../users-data";
 import { TUser } from "../users-data";
 
@@ -6,14 +6,25 @@ interface IUserProps {
   data: {
     firstName: string;
     lastName: string;
+    id: number;
   };
+  index: number;
+  maxIndex: number;
+  handleDelete: (userId: number) => void;
+  handleMoveUp: (index: number, n: 1 | -1) => void;
 }
 
 const User = (props: IUserProps) => {
-  const { data } = props;
+  const { data, index, maxIndex, handleDelete, handleMoveUp } = props;
 
-  // TODO: add delete button to each of the user
-  // TODO: implement logic to delete user
+  const [isLike, setIsLike] = useState<boolean>(false);
+
+  const handleToggleLike = () => {
+    setIsLike(prev => !prev);
+  }
+
+  // TODO:+add delete button to each of the user
+  // TODO: +implement logic to delete user
 
   // TODO: add a Like button to each of the user
   // TODO: add a state to keep liked state - is user liked or not (true/false) - useState, default value false
@@ -22,9 +33,14 @@ const User = (props: IUserProps) => {
 
   return (
     <li>
+      <button disabled={index <= 0} onClick={() => handleMoveUp(index, -1)}>Up</button>
+      <button disabled={index >= maxIndex} onClick={() => handleMoveUp(index, 1)}>Down</button>
       <span>
         {data.firstName} {data.lastName}
+        {isLike && <span>💝</span>}
       </span>
+      <button onClick={() => handleToggleLike()}>Like</button>
+      <button onClick={() => handleDelete(data?.id)}>delete</button>
     </li>
   );
 };
@@ -32,26 +48,41 @@ const User = (props: IUserProps) => {
 export function Users() {
   const [users, setUsers] = React.useState<TUser[]>(usersData);
 
-  const handleDelete = (user: TUser) => {
-    console.log("deleting", user);
-    // TODO: implement function to delete user
-    // TODO: create new list of users without deleted user
-    // TODO: call setUsers with new list of users
+  const handleDelete = (userId: number) => {
+    // console.log("deleting", userId);
+    setUsers((prev => prev.filter(item => item.id !== userId)));
+    // TODO: +implement function to delete user
+    // TODO: +create new list of users without deleted user
+    // TODO: +call setUsers with new list of users
   };
-  console.log(handleDelete, setUsers);
+  // console.log(handleDelete, setUsers);
 
-  // TODO: pass handleDelete to User component
+  // TODO: +pass handleDelete to User component
 
-  // TODO: Add "Move Up" and "Move Down" buttons to each of the user
-  // TODO: Implement functions to move user up/down the list
-  // TODO: Make sure you create new list of users, do not mutate existing list
-  // TODO: Call setUsers with new list of users
-  // TODO: Pass handleMoveUp and handleMoveDown to User component as props
+  // TODO: +Add "Move Up" and "Move Down" buttons to each of the user
+  // TODO: +Implement functions to move user up/down the list
+  // TODO: +Make sure you create new list of users, do not mutate existing list
+  // TODO: +Call setUsers with new list of users
+  // TODO: +Pass handleMoveUp and handleMoveDown to User component as props
+  const handleMoveUp = (index: number, n: 1 | -1) => {
+    const newUsers = [...users];
+
+    [newUsers[index], newUsers[index + n]] = [newUsers[index + n], newUsers[index]];
+
+    setUsers(newUsers)
+  };
 
   return (
     <ul>
-      {users.map((user) => (
-        <User data={user} key={user.id} />
+      {users.map((user, index) => (
+        <User
+          data={user}
+          index={index}
+          key={user.id}
+          maxIndex={users.length - 1}
+          handleDelete={handleDelete}
+          handleMoveUp={handleMoveUp}
+        />
       ))}
     </ul>
   );
