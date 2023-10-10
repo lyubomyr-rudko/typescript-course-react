0c4e05237d09ec133be38722a84f6c305446b772
Victor Davydenko <acc.davydenko@gmail.com>
2023-10-10 20:41:55 +0300

    hw13 done

--- src/lesson13/Users-homework.styled.tsx
+++ src/lesson13/Users-homework.styled.tsx
@@ -1,5 +1,73 @@
 import styled from "styled-components";
 
+interface IColorDictionary {
+    [key: string] : string
+}
+
+interface IHairColorPreviewProps {
+    color: string
+}
+
 export const Container = styled.div`
   display: flex;
-`;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  max-width: 1000px;
+  margin: 0 auto;
+`
+
+export const Table = styled.table`
+  width: 100%;
+  text-align: left;
+  font-weight: 500;
+`
+
+export const THeadCell = styled.th`
+  padding-bottom: 30px;
+  color: gray;
+  width: calc(100% / 5);
+`
+
+export const TableRow = styled.tr`
+  position: relative;
+  
+  &:after {
+    content: '';
+    width: 110%;
+    height: 1px;
+    background: #eee;
+    position: absolute;
+    bottom: 0;
+    left: -5%;
+  }
+`
+
+export const TableCell = styled.td`
+  padding: 15px 0;
+  vertical-align: center;
+`
+
+export const Heading = styled.h1`
+  color: gray;
+  font-size: 24px;
+  align-self: flex-start;
+  margin-bottom: 50px;
+`
+
+
+const colorsDictionary: IColorDictionary = {
+    Blond: '#FFFF00',
+    Brown: '#964B00',
+    Black: '#000',
+    Chestnut: '#C4A484',
+    Auburn: '#C04000',
+}
+
+export const HairColorPreview = styled.span<IHairColorPreviewProps>`
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background: ${({ color }: IHairColorPreviewProps) => colorsDictionary[color]};
+`
--- src/lesson13/Users-homework.tsx
+++ src/lesson13/Users-homework.tsx
@@ -1,8 +1,14 @@
 import usersData from "../users-data";
 import { TUser } from "../users-data";
+import {
+  TableRow,
+  TableCell,
+  HairColorPreview
+} from "./Users-homework.styled.tsx";
+import {getDateString} from "./utils.ts";
 
 interface IUserProps {
-  data: TUser;
+  user: TUser;
 }
 // TODO: Update User component to display user's name, Gender, Hair color, Birth dat and phone number
 // TODO: Style this component using styled-components
@@ -11,17 +17,24 @@ interface IUserProps {
 // TODO: Add a color prop to HairColorIcon, so it can be used to display different colors
 // TODO: Add your styled-components to src/lesson13/Users-homework.styled.tsx
 
-const User = (props: IUserProps) => {
-  const { data } = props;
-
+const UserRow = ({ user }: IUserProps) => {
+  const { firstName, lastName, birthDate, gender, hair: {color}, phone} = user
+  const phoneToDisplay: string = phone.replace(/[+' ']/g,'')
+  const birthDateToDisplay: string = getDateString(birthDate)
   return (
-    <li>
-      {data.firstName} {data.lastName}
-    </li>
+    <TableRow>
+      <TableCell>{firstName} {lastName}</TableCell>
+      <TableCell>{gender}</TableCell>
+      <TableCell>
+        <HairColorPreview color={color}></HairColorPreview>
+      </TableCell>
+      <TableCell>{birthDateToDisplay}</TableCell>
+      <TableCell>{phoneToDisplay}</TableCell>
+    </TableRow>
   );
 };
 
-export function Users() {
+const TableBody = () => {
   // TOOD: update this component to display a header and a list of users
   // User Name | Gender | Hair Color | Birth date | Phone number
   // TODO: Style this component using styled-components
@@ -29,12 +42,12 @@ export function Users() {
   // TODO: Add your styled-components to src/lesson13/Users-homework.styled.tsx
 
   return (
-    <ul>
+      <tbody>
       {usersData.map((user) => (
-        <User data={user} key={user.id} />
+          <UserRow user={user} key={user.id} />
       ))}
-    </ul>
+      </tbody>
   );
 }
 
-export default Users;
+export default TableBody;
