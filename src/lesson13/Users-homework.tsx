5972d8db36b57ddd54809fe6c4a1c2f59cb064f4
Serhii Onopriienko <seriyrayne@gmail.com>
2023-10-11 12:08:52 +0200

    homeWork13

--- src/lesson13/Users-homework.tsx
+++ src/lesson13/Users-homework.tsx
@@ -1,39 +1,43 @@
 import usersData from "../users-data";
 import { TUser } from "../users-data";
+import { Container, HeadText, HeaderList, HeaderSublist, UnorderedList, List, HairColor } from "./Users-homework.styled";
 
 interface IUserProps {
   data: TUser;
 }
-// TODO: Update User component to display user's name, Gender, Hair color, Birth dat and phone number
-// TODO: Style this component using styled-components
-// TODO: Use Users-homework.jpg or Users-homework.fig as a reference
-// TODO: Add a component to display user's hair color as a colored circle HairColorIcon
-// TODO: Add a color prop to HairColorIcon, so it can be used to display different colors
-// TODO: Add your styled-components to src/lesson13/Users-homework.styled.tsx
 
 const User = (props: IUserProps) => {
   const { data } = props;
+  const birthDate = new Intl.DateTimeFormat('en-GB', { dateStyle: 'full', timeStyle: 'long', timeZone: 'Australia/Sydney' }).format(new Date(data.birthDate)).split(' ');
 
   return (
-    <li>
+    <>
+    <List>
       {data.firstName} {data.lastName}
-    </li>
+    </List>
+    <List>{data.gender}</List>
+    <List><HairColor hairColor={data.hair.color} /></List>
+    <List>{birthDate[1]} {birthDate[2]}, {birthDate[3]}</List>
+    <List>{data.phone}</List>
+    </>
   );
 };
 
 export function Users() {
-  // TOOD: update this component to display a header and a list of users
-  // User Name | Gender | Hair Color | Birth date | Phone number
-  // TODO: Style this component using styled-components
-  // TODO: Use Users-homework.jpg or Users-homework.fig as a reference
-  // TODO: Add your styled-components to src/lesson13/Users-homework.styled.tsx
-
   return (
-    <ul>
+    <Container>
+    <HeadText>Users</HeadText>
+    <HeaderList>
+      <HeaderSublist>User Name</HeaderSublist>
+      <HeaderSublist>Gender</HeaderSublist>
+      <HeaderSublist>Hair Color</HeaderSublist>
+      <HeaderSublist>Birth Date</HeaderSublist>
+      <HeaderSublist>Phone Number</HeaderSublist>
+    </HeaderList>
       {usersData.map((user) => (
-        <User data={user} key={user.id} />
-      ))}
-    </ul>
+        <UnorderedList><User data={user} key={user.id} /></UnorderedList>
+        ))}
+    </Container>
   );
 }
 
