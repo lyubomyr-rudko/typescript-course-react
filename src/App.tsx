618589bda85b45c71c7c1d3f5c0446e67144905c
Serhii Revva <besha2vox@gmail.com>
2023-10-29 00:45:13 +0300

    complete hw#15

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import "./App.css";
-import { Lesson17Homework } from "./lesson17-homework";
+import './App.css';
+import UseReducerDemo from './lesson15-homework/useReducer';
+import UseEffectDemo from './lesson15-homework/useEffect';
+import UseStateDemo from './lesson15-homework/useState';
+import CastomHookDemo from './lesson15-homework/customHook';
 
-export function App() {
-  return (
-    <>
-      <Lesson17Homework />
-    </>
-  );
-}
+const Line = () => {
+    return (
+        <span style={{ width: '100%', height: 1, backgroundColor: '#fff' }} />
+    );
+};
+
+export const App = () => {
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 12 }}>
+            <UseEffectDemo />
+            <Line />
+            <UseReducerDemo />
+            <Line />
+            <UseStateDemo />
+            <Line />
+            <CastomHookDemo />
+        </div>
+    );
+};
 
 export default App;
